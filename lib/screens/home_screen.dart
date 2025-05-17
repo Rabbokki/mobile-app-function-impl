3435bc996195800f92dfcbc925cf9adc9dae53d2
hmjlon <jiyoung110077@gmail.com>
2025-05-17 14:25:40 +0900

    지금까지 한 거

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,15 +6,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFEF5FF), // 연보라색 배경
-      body: Column(
-        children: [
-          Image.asset(
-            'assets/images/heroBanner2.png',
-            fit: BoxFit.cover,
-          ),
+      backgroundColor: const Color(0xFFFEF5FF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/heroBanner2.png',
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20),
 
-        ],
+            // 항공권 이동 버튼
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/flight');
+              },
+              child: const Text('항공권 검색'),
+            ),
+
+            const SizedBox(height: 10),
+
+            // 추천 명소 이동 버튼
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/recommended');
+              },
+              child: const Text('추천 명소 보기'),
+            ),
+          ],
+        ),
       ),
     );
   }
